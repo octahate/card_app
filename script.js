49360a68fbdd700f8f04b33f16895282923ecf25
octahate <geli961212@hotmail.com>
2023-12-31 16:10:17 +0100

    upődae

---
+++ script.js
@@ -0,0 +1,114 @@
+let decks = {
+    "deck1": [
+        "Short Text 1",
+        "Another Short Text",
+        "Yet Another One",
+        "Short Text Again",
+        "Brief Text 5"
+    ],
+    "deck2": [
+        "Longer Text Example 1: This is a longer piece of text to test how the cards handle more content.",
+        "Longer Text Example 2: Another long sentence that stretches the content of the card.",
+        "A third long sentence that is meant to test the capacity of the card to handle more text."
+    ],
+    "deck3": [
+        "Medium Length Text 1",
+        "Text with Medium Length 2",
+        "Another Medium Text",
+        "Medium Text Again",
+        "Last Medium Text"
+    ],
+    "deck4": [
+        "Mix 1: Short",
+        "Mix 2: A bit longer text to mix things up.",
+        "Mix 3: Short Again",
+        "Mix 4: This one is a little bit longer to see the effect on the card.",
+        "Mix 5: Short"
+    ],
+    "Just One": [
+        "film series, nest, spice, elephant, carnival","grill, panda, dream, marriage, navel","mirror, prom, bathtub, mine, fairy","mine, rubber, pan, lightning, tunnel","butterfly, powder, porcelain, market, doll","mummy, battery, fair, karate, carrot","screw, rake, computer, Tesco, ball","thunder, garden, painting, hut, ceasefire","single, potato, wave, school, Lego","Xbox, bet, salt, manual, freeze","register, PlayStation, blonde, bridal veil, thigh","rat, catcher, Amazon, tobacco, ruler","Halloween, chip, baby, canteen, pair","crane, musketeer, thread, tulip, IKEA","oven, socket, Easter, slag, rail","club, Christmas, fashion, station, lamp","rod, operation, text, muse, protection","king, ballet, band, alcohol, lava","revenge, boss, emergency, cross, waltz","number, decathlon, rum, metal, tuna","myth, Facebook, acorn, bone, bridge","Google, bow, Mars, golf, ticket","mafia, comprehensive, rocket, Microsoft, ski","canvas, vampire, Monopoly, manga, hotel","failure, history, beer, disco, president","towel, Venus, octopus, circle, opera","tango, graduation, temple, sand, french fries","reggae, manure, lake, monkey, lighthouse","neighbor, rock, tiger, needle, soap","prison, hole, punk, evening, map","cave, finger, elf, poker, cake","sugar, theater, shovel, dune, pregnant","cat, palace, election, honey, Rambo","goat, bread, devil, base, Tarzan","gorilla, uniform, rain, fire, helmet","iron, Batman, song, Nile, cinema","sailboat, Camembert, Shrek, pole, west","Barbie, chocolate, snow, draw, wind","Viking, alarm, dance, Pinocchio, desert","ice, whistle, champagne, safari, alien","Spartacus, coat, tornado, pyramid, alliance","drawer, Snow White, pancake, island, taxi","plastic, ray, cardboard, pebble, Everest","Terminator, leaf, girl, tomato, egg","house, Godfather, war, rope, wine","hammer, Chile, goulash, emperor, pope","pastry, stallion, Mexico, white, bald","tractor, humor, corner, Antarctica, cold cuts","doping, garlic, cube, beech, milk","board, firefighter, glass, couscous, Greece","Hollywood, mosquito, spear, wallet, end","Avatar, mill, jungle, nun, bankruptcy","pump, Aladdin, pipe, belt, bar","mouth, merry-go-round, soul, grass, waterfall","radio, pear, gladiator, sun, bran","berry, examination, Goth, Titanic, blender","ladder, angel, evil, hair, matrix","string, Australia, castle, guard, sheep","puppet, game, Vegas, safe, airplane","brain, mask, concert, Troy, shark","pepper, triangle, doll, Russia, scene","polar bear, mouse, necklace, farm, Belgium","France, moon, buffet, handle, tool","Tokyo, high heels, chicken, helicopter, colonel","heart, Moscow, pool, unicorn, orange","melon, anchor, Israel, cactus, tennis","music, turkey, America, coffee, fever","knight, peace, flower, Switzerland, calendar","slipper, foliage, puma, revolution, Sahara","Caesar, pasta, hat, dentist, wheat","shell, Shakespeare, giant, foam, cavity","knife, pillow, Orban, sword, flight","light, sofa, Mozart, ring, pizza","tradition, snake, cup, Picasso, watch","guillotine, wizard, hockey, banana, Gyurcsany","carpet, pony, crown, new, Tarantino","Lazar, ninja, pacifier, hunter, vegetable","hall, Ford, casino, lollipop, well","mirage, Ramesses, earth, crossroads, princess","ship, bowling, Churchill, goat, spy","Olympia, mountaineer, lawyer, Örkény, river","mustache, bubble, chain, Adam's costume, cookie"
+    ],
+    "Jó tudni": [
+        "Milyen hosszú lenne a számodra tökéletes földközi-tengeri hajóút?","Hányszor cseréltél kereket életedben? (biciklin, motoron vagy autón)","Hány olyan párkapcsolatod volt, ami tovább tartott, mint egy év?","Hány különböző dolgot gyűjtesz?","Mióta gyakorlód a kedvenc hobbidat?","Mennyi pénzért lennél hajlandó egy éven keresztül egy antarktiszi kutatóközpontban dolgozni?","Hányszor tudnád hibátlanul kimondani gyorsan egymás után, hogy „Jamaica a jamaicaiaké?","Ha egy napra egy vízparti helyre utazol, mennyi időt töltesz ideális esetben a strandon?","Hány éjszaka volt a legtöbb, amit egyhuzamban egy sátorban töltöttél el?","Hány cserepes virágod van otthon?","Hány különböző parfümöd van jelenleg?","Hetente hány alkalommal főzöl? 1","Hány napig tartott életed leghosszabb nyaralása?","Szerinted hány M&M's drazsét tudnál egyszerre a szádba tömni?","Hány múzeumban jártál az elmúlt egy évben?","Maximum mennyit lennél hajlandó fizetni egy fogásért egy igazán előkelő étteremben?","Hány csésze kávét és/vagy teát ittál eddig ma?","Hány kalapod és sapkád van összesen?","Hány négyzetméteres a számodra ideális otthon (az udvart nem számolva)?","Ha nyernél egy all-inclusive nyaralást egy magánszigetre, hány napig szeretnél ott tartózkodni?","Hány évet áldoznál fel az életedből azért, hogy tudj teleportálni?","Ha a teljes utazásod (ideértve a jegyeket és a szállást is) ingyen lenne, évente hányszor látogatnál el Disneylandbe?","Hány másodpercig tudod visszatartani a lélegzetedet?","A többi játékos közül hány embert tudnál legyőzni Scrabble-ben?","Hánykor kelsz hétköznapokon?","Ha nyernél egy utazást a Holdra, mennyi ideig szeretnéd, hogy tartson?","Milyen értékű volt a legdrágább tárgy, amit valaha tönkretettél?","Hány pár cipőd van?","Hány tetoválásod van?","Hányszor voltál síelni vagy snowboardozni?","Hány évesen kezdtél el dolgozni?","Hány éves korodból származik a legkorábbi emléked?","Ha rajtad múlna, évente hány napot esne a hó?","Mennyi időt fordítasz reggelente a megjelenésedre? (ruha, smink, frizura, borotválkozás stb.)","Hány keresztneved van?","Hetente hányszor mész élelmiszerboltba vásárolni?","Hány különböző sportot űztél legalább egy évig az iskolaikon kívül?","Havonta hányszor eszel gyorsétteremben?","Hány unokatestvéred van?","Hány percig tudod tartani az egyensúlyodat fél lábon?","Maximum mennyit fizetnél egy olyan vidámparki belépőért, ahol igazi, élő dinoszauruszokat csodálhatsz meg?","Maximum mennyit fizetnél azért, hogy a kedvenc zenekarod a lakásodban lépjen fel?","Évente hány nap szabadság az ideális számodra?","Hány gyereket szeretnél?","Szerinted hány másodpercig bírod pislogás nélkül?","Maximum mennyit költenél egy egyhetes űrutazásra?","A többi játékos közül hány embert győznél le 100 méteres síkfutásban?","Hányszor láttad a Titanicot?","Hetente általában hány étkezésed tartalmaz húst?","Ha most kapnál 20 millió forintot, mennyi időbe telne, hogy mindet elköltsd?","Hetente általában hány pohár alkoholos italt fogyasztasz? (bor, sör, koktél, pálinka stb.)","Ha életed végéig ugyanolyan idős maradhatnál, hány éves lennél?","Hány alkalmazás van letöltve a telefonodra?","Évente hányszor mész moziba?","Ha egy barátod késik a megbeszélt találkozóról, mennyit vársz, mielőtt felhívod vagy ráírsz?","Zeneileg melyik évtized volt a legjobb a XX. században?","Jelenleg hány üveg erős szeszes italod van?","Általában hány perccel korábban érkezel egy fontos találkozóra a megbeszélthez képest?","Hetente általában hány filmet nézel meg?","Havonta hányszor mész étterembe (az elviteles vagy a gyorséttermi rendeléseket nem számolva)?","Havonta hányszor szoktál edzeni?","Mennyi a maximális idő, amit hajlandó vagy várni egy szabad asztalra egy étteremben?","Általában hány percig zuhanyozol?","Hány különböző nyelven tudod mondani: „Szeretlek?","Melyik évet találod a legérdekesebbnek a történelemből?","Hány éves korodban költöztél külön a szüléidtől?","Hány éve tart a leghosszabb barátságod?","Évente hányszor mész fodrászhoz?","Hány évet áldoznál fel az életedből azért, hogy azonnal és tökéletesen megtanulj játszani egy általad választott hangszeren?","Szerinted milyen messzire tudnád eldobni a cipődet?","Hány kontinensen jártál? (Afrika, Antarktisz, Ausztrália, Ázsia, Dél-Amerika, Európa, Észak-Amerika)","Hány fekvőtámaszt tudsz megcsinálni egyhuzamban?","Általában hány könyvet olvasol el egy évben?","Hány nyelven tudsz káromkodni?","Ha semmilyen korlátozás nem szólna közbe, hány házi kedvencet tartanál?","Hány párnán alszol?","Hány különböző helyen laktál eddig életedben?","Általában hánykor kelsz hétvégén?","Hány társasjátékod van?","Mióta laksz a jelenlegi otthonodban?","Naponta hányszor látogatod meg a fürdőszobát?","Hány perc alatt érsz be a munkahelyedre/suliba?","Hány évet áldoznál fel az életedből azért, hogy azonnal milliárdossá válj?","Milyen hosszú a tökéletes délutáni szundi?","Mit gondolsz, hány embert tudnál legyőzni szkanderben a többi játékos közül?","Hány évesnek érzed magad?","Hányas lábad van?","Ha választhatnál, milyen magas lennél?","Ha utazhatnál az időben, és egy teljes napot eltölthetnél egy másik korban, melyik évet választanád?","Mennyi ideig tartott a leghosszabb párkapcsolatod?","Általában hány órakor mész aludni hétköznap esténként?","Hány szoba van az otthonodban?","Hány ismerősöd van a Facebookon?","Mennyi pénzt kellene most azonnal kapnod ahhoz, hogy anyagilag biztonságban érezd magad?","Szerinted hány éves korodig fogsz élni?","Hány óra az ideális alvásmennyiség számodra?","Hány kulcs van a kulcscsomódon?","Hány évet adnál az életedből azért, hogy a világ összes nyelvén tudj beszélni?","Az első reggeli óracsörgés után mennyi idővel kelsz ki az ágyból?","Általában mennyi készpénzt hordasz magadnál?"
+        ]
+};
+
+let currentDeck = [];
+let currentIndex = 0;
+
+// Function to shuffle the deck
+function shuffleDeck(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// Function to open a specific deck
+function openDeck(deckName) {
+    currentDeck = [...decks[deckName]]; // Clone the deck
+    shuffleDeck(currentDeck);
+    currentIndex = 0;
+    displayCard();
+    document.getElementById("menu").classList.add("hidden");
+    document.getElementById("cardViewer").classList.remove("hidden");
+}
+
+// Function to display the current card
+function displayCard() {
+    document.getElementById("card").innerText = currentDeck[currentIndex];
+    document.getElementById("cardPosition").innerText = (currentIndex + 1) + "/" + currentDeck.length;
+}
+
+// Function to show the next card
+function showNextCard() {
+    if (currentIndex < currentDeck.length - 1) {
+        currentIndex++;
+        displayCard();
+    }
+}
+
+// Function to show the previous card
+function showPreviousCard() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        displayCard();
+    }
+}
+
+// Function to reshuffle the current deck
+function reshuffleDeck() {
+    if (confirm("Are you sure you want to reshuffle the cards?")) {
+        shuffleDeck(currentDeck);
+        currentIndex = 0;
+        displayCard();
+    }
+}
+
+// Function to toggle the language - Placeholder for now
+function toggleLanguage() {
+    alert("Language toggled!");
+}
+
+// Function to return to the menu
+function returnToMenu() {
+    document.getElementById("menu").classList.remove("hidden");
+    document.getElementById("cardViewer").classList.add("hidden");
+}
+
+// Function to generate the menu dynamically
+function generateMenu() {
+    const menu = document.getElementById("deckMenu");
+    menu.innerHTML = ""; // Clear existing menu items
+
+    for (const deckName in decks) {
+        const deckItem = document.createElement("li");
+        deckItem.innerText = deckName; // Extracting the number from deckName
+        deckItem.onclick = function() { openDeck(deckName); };
+        menu.appendChild(deckItem);
+    }
+}
+
+// Call generateMenu when the script loads
+generateMenu();
+
